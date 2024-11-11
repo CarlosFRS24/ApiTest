a68bb4d8796d01654c4e8169fe341c49514a02a1
CarlosFRS24 <carl.roman@duocuc.cl>
2024-11-11 19:55:41 -0300

    xd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const router = jsonServer.router("almacen.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 10000;
 
-server.unsubscribe(middlewares);
+server.use(middlewares);
 server.use(router);
 
 server.listen(port);
